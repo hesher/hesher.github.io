1dc852f21542b407f1112ea3673cf7bb1ffdb8d1
hesher <ben.katz@gmail.com>
2015-01-02 10:11:52 +0200

    add ui.router

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 /*globals angular */
 /*jslint unparam: true, browser:true */
 
-var app = angular.module('PersonalWebsite', ['ui.bootstrap']);
+var app = angular.module('PersonalWebsite', ['ui.bootstrap', 'ui.router']);
 
 function transformElm(elm, pTrans) {
   'use strict';
@@ -12,23 +12,60 @@ function transformElm(elm, pTrans) {
   elm.style.transform = pTrans;
 }
 
+app.config(function ($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise('/front');
+  //
+  // Now set up the states
+  //
+  function genDialog(url) {
+    return ['$stateParams', '$state', '$modal', function ($stateParams, $state, $modal) {
+        $modal.open({
+            templateUrl: url,
+            // resolve: {
+            //   item: function () { new Item(123).get(); }
+            // },
+            controller: ['$scope', function ($scope) {
+              $scope.dismiss = function () {
+                $scope.$dismiss();
+              };
+            }]
+        }).result.then(function (result) {
+          if (result) {
+            return $state.transitionTo('front');
+          }
+        }, function () {
+          return $state.transitionTo('front');
+        });
+      }];
+  }
+  $stateProvider
+    .state('front', {
+      url: '/front',
+      templateUrl: 'index.html'
+    })
+    .state('tamar', {
+      url: '/tamar',
+      onEnter: genDialog('partials/tamar.html')
+    })
+    .state('kids', {
+      url: '/kids',
+      onEnter: genDialog('partials/kids.html')
+    })
+    .state('infants', {
+      url: '/infants',
+      onEnter: genDialog('partials/infants.html')
+    })
+    .state('article', {
+      url: '/article',
+      onEnter: genDialog('partials/story-telling.html')
+    });
+});
+
 app.controller('MainCtrl', ['$scope', '$modal', function ($scope, $modal) {
   'use strict';
-  var dialogs = {
-    infants: '/views/infants.html',
-    kids: '/views/kids.html',
-    tamar: '/views/tamar.html',
-    article: '/articles/story-telling.html'
-  };
 
-  $scope.open = function (which) {
-    console.log(dialogs[which]);
-    var instance = $modal.open({
-      templateUrl: dialogs[which],
-      scope: $scope
-    });
-
-  };
 }]);
 
 app.directive('jiggly', ['$interval', function ($interval) {
